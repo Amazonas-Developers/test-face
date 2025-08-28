e2c79a21335505c6803eb6bdb525b49224b3690b
Yaseni Sojo <ysojo@gastroturismoherschel.local>
2025-08-28 14:37:20 -0400

    update

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ import os
 import cv2
 from dotenv import load_dotenv
 from face_encoder import FaceEncoder
+import face_recognition
+import numpy as np
 
 
 load_dotenv()
 
 api_key = os.getenv('url_amazona')
-url_dvr_local = f'{api_key}/Streaming/channels/101/'
+url_dvr_local = f'{api_key}/Streaming/channels/202/'
 
 
 
@@ -17,9 +19,8 @@ cap = cv2.VideoCapture(url_dvr_local)
 
 
 
-
 if not cap.isOpened():
-    print(f"Error: No se pudo conectar al stream del DVR en la URL: {url_nvr_orlando}")
+    print(f"Error: No se pudo conectar al stream del DVR en la URL: {url_dvr_local}")
     print("Verifica si la dirección IP, el puerto, el usuario y la contraseña son correctos.")
     print("También, la 'ruta_del_stream' (/cam/realmonitor?...) es muy dependiente del fabricante y podría ser incorrecta.")
     exit()
@@ -28,21 +29,23 @@ if not cap.isOpened():
 
 # Bucle principal para leer y mostrar los fotogramas del stream.
 while True:
-    # Lee un fotograma.
     ret, frame = cap.read()
-
-    # Si la lectura del fotograma falla (ej. se pierde la conexión), sal del bucle.
     if not ret:
         print("El stream se ha detenido.")
         break
 
-    # Muestra el fotograma en una ventana de OpenCV.
     cv2.imshow('Stream de DVR', frame)
 
-    # Espera 1 milisegundo. Si se presiona la tecla 'q', sal del bucle.
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 # Libera el objeto de captura y cierra todas las ventanas.
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
